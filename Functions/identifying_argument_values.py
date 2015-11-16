7fb18fe72aaea43e5dde3f54ed2c179869bf6054
Pilipović Ivan <pilipovicivan@yahoo.com>
2015-11-16 02:54:30 +0100

    Updated example
    Updated example from Chapter 3

--- Functions/identifying_argument_values.py
+++ Functions/identifying_argument_values.py
@@ -1,6 +1,6 @@
 import inspect
 
-def example(a=1, b=1, *c, d, e=2, **f) -> str:
+def example(a=1, b=1, *c, d, e=2, **f):
 	pass
 
 def get_arguments(func, args, kwargs):
@@ -12,6 +12,12 @@ def get_arguments(func, args, kwargs):
 	arguments = kwargs.copy()
 	spec = inspect.getfullargspec(func)
 	arguments.update(zip(spec.args, args))
+
+	if spec.defaults:
+		for i, name in enumerate(spec.args[-len(spec.defaults):]):
+			if name not in arguments:
+				arguments[name] = spec.defaults[i]
+
 	return arguments
 
 print(get_arguments(example, (1,), {'f':4}))
